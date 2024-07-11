e3a24fd6f01c3da013d892a33a1c0c55bd3c9f64
Armel Bogue <komlanbogue@gmail.com>
2024-07-11 13:52:48 +0000

    End

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const currDisplay = document.querySelector('.curr-display');
 const prevDisplay = document.querySelector(".prev-display");
-const num = document.querySelectorAll('.num');
+const nums = document.querySelectorAll('.num');
 const operands = document.querySelectorAll('.operation');
 const clearBtn = document.querySelector(".clear");
 const delBtn = document.querySelector('.del');
@@ -9,16 +9,14 @@ const equalBtn = document.querySelector('.equal');
 let operation;
 
 function appendNumber(num){
-    if (num === "." && currDisplay.innerText.includes(".")) return; {
-        currDisplay.innerText += num;
-    }
+    if (num === "." && currDisplay.innerText.includes(".")) return;
+    currDisplay.innerText += num;
 }
 
-function chooseOperation (operands){
+function chooseOperation (operand){
     if (currDisplay.innerText === "") return;
-    compute(operands);
-    operation = operands;
-    currDisplay.innerText += operands;
+    operation = operand;
+    currDisplay.innerText += operand;
     prevDisplay.innerText = currDisplay.innerText;
     currDisplay.innerText = "";
 }
@@ -28,3 +26,52 @@ function clearDisplay(){
     prevDisplay.innerText = "";
 }
 
+function compute() {
+    let result;
+    const previousValue = parseFloat(prevDisplay.innerText);
+    const currentValue = parseFloat(currDisplay.innerText);
+
+    if (isNaN(previousValue) || isNaN(currentValue)) return;
+
+    switch (operation) {
+        case "+":
+            result = previousValue + currentValue;
+            break;
+        case "-":
+            result = previousValue - currentValue;
+            break;
+        case "*":
+            result = previousValue * currentValue;
+            break;
+        case "/":
+            result = previousValue / currentValue;
+            break;
+        default:
+            return;    
+    }
+
+    currDisplay.innerText = result;
+    prevDisplay.innerText = "";
+}
+
+nums.forEach((num) => {
+    num.addEventListener("click", () => {
+        appendNumber(num.innerText);
+    });
+});
+
+operands.forEach((operand) => {
+    operand.addEventListener("click", () => {
+        chooseOperation(operand.innerText);
+    });
+});
+
+clearBtn.addEventListener("click", () => {
+    clearDisplay();
+});
+
+equalBtn.addEventListener("click", compute);
+
+delBtn.addEventListener("click", () => {
+    currDisplay.innerText = currDisplay.innerText.slice(0, -1);
+});
